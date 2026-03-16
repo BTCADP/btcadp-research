0dfe92b21a8348dcb3a791c9ae31568acefd36fe
BTCADP <btcadp@protonmail.com>
2026-03-15 21:51:40 -0400

    Update website

--- push.bat
+++ push.bat
@@ -1,5 +1,6 @@
 cd "C:\Users\mrjun\OneDrive\Documents\BTCC (Bitcoin Currency)\Website\btcadp-research"
 git add .
 git commit -m "Update website"
+git pull --rebase
 git push
 npx wrangler pages deploy . --project-name btcadp
